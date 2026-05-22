7af5ef198eda4789dea4b06ecc29cf195bbbeb80
stefanymts <stefany.martins04@escola.pr.gov.br>
2026-05-22 09:10:26 -0300

    Update HomeScreen.js

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -174,7 +174,7 @@ const [carregando, setCarregando] = useState(false);
 const sincronizarReceitas = async () => {
 setCarregando(true);
 try {
-const response = await fetch('https://raw.githubusercontent.com/anajuglari/Mobile/refs/heads/main/receitas.json?raw=true');
+const response = await fetch('https://raw.githubusercontent.com/stefanymts_/Mobile/refs/heads/main/receitas.json?raw=true');
 const receitasDoServidor = await response.json();
 const novas = receitasDoServidor.filter(resServidor => !receitas.some(resLocal => resLocal.id === resServidor.id));
 
